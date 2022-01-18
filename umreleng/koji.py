69dabfa05a071befa421e65233f590d8285f846f
Cappy Ishihara <cappy@cappuchino.xyz>
2022-01-18 19:07:29 +0700

    reformat with black

--- umreleng/koji.py
+++ umreleng/koji.py
@@ -5,53 +5,57 @@ import threading
 import time
 import umreleng.logger as logger
 import sys
+
+
 class KojiWatcher(threading.Thread):
     def __init__(self, task_id):
         threading.Thread.__init__(self)
         self.task = task_id
         self.logger = logger.logger
-        self.name = f'Kojiwatcher for task {self.task}'
-        self.koji = koji.ClientSession('https://lapis.ultramarine-linux.org/kojihub')
+        self.name = f"Kojiwatcher for task {self.task}"
+        self.koji = koji.ClientSession("https://lapis.ultramarine-linux.org/kojihub")
+
     def run(self, *args, **kwargs):
         task_state = object
         while self.is_alive():
             task = self.koji.getTaskInfo(self.task)
-            if task['state'] == koji.TASK_STATES['FREE']:
+            if task["state"] == koji.TASK_STATES["FREE"]:
                 # check task_state if it's still free
-                if task_state == koji.TASK_STATES['FREE']:
+                if task_state == koji.TASK_STATES["FREE"]:
                     # if task is still free, wait for a bit and check again
                     time.sleep(5)
                 else:
-                    task_state = koji.TASK_STATES['FREE']
-                    self.logger.info(f'Task {self.task} is now free')
+                    task_state = koji.TASK_STATES["FREE"]
+                    self.logger.info(f"Task {self.task} is now free")
                     time.sleep(5)
                 continue
-            elif task['state'] == koji.TASK_STATES['OPEN']:
+            elif task["state"] == koji.TASK_STATES["OPEN"]:
                 # check task_state if it's still open
-                if task_state == koji.TASK_STATES['OPEN']:
+                if task_state == koji.TASK_STATES["OPEN"]:
                     # if task is still open, wait for a bit and check again
                     time.sleep(5)
                 else:
-                    task_state = koji.TASK_STATES['OPEN']
-                    self.logger.info(f'Task {self.task} is now open')
+                    task_state = koji.TASK_STATES["OPEN"]
+                    self.logger.info(f"Task {self.task} is now open")
                     time.sleep(5)
-            elif task['state'] == koji.TASK_STATES['CLOSED']:
-                self.logger.info(f'Task {self.task} is closed')
+            elif task["state"] == koji.TASK_STATES["CLOSED"]:
+                self.logger.info(f"Task {self.task} is closed")
                 self.stop()
-            elif task['state'] == koji.TASK_STATES['ASSIGNED']:
-                if task_state == koji.TASK_STATES['ASSIGNED']:
+            elif task["state"] == koji.TASK_STATES["ASSIGNED"]:
+                if task_state == koji.TASK_STATES["ASSIGNED"]:
                     # if task is still assigned, wait for a bit and check again
                     time.sleep(5)
                 else:
-                    task_state = koji.TASK_STATES['ASSIGNED']
-                    self.logger.info(f'Task {self.task} is now assigned')
+                    task_state = koji.TASK_STATES["ASSIGNED"]
+                    self.logger.info(f"Task {self.task} is now assigned")
                     time.sleep(5)
-            elif task['state'] == koji.TASK_STATES['FAILED']:
-                self.logger.info(f'Task {self.task} has failed')
+            elif task["state"] == koji.TASK_STATES["FAILED"]:
+                self.logger.info(f"Task {self.task} has failed")
                 self.stop()
             else:
                 self.logger.info(f'Task {self.task} is in state {task["state"]}')
                 time.sleep(5)
+
     def stop(self):
         self.running = False
         sys.exit(0)
--- umreleng/logger.py
+++ umreleng/logger.py
@@ -7,27 +7,36 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 
 # add prefix
-formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 # log to file
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 # log to file
-#logfile = logging.FileHandler('releng.log')
-#logfile.setFormatter(formatter)
-#logger.addHandler(logfile)
+# logfile = logging.FileHandler('releng.log')
+# logfile.setFormatter(formatter)
+# logger.addHandler(logfile)
 def addLogger(logger_name, logfile):
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     # set the logger name
     logger.addHandler(handler)
 
+
 def info(message):
     logger.info(message)
+
+
 def debug(message):
     logger.debug(message)
+
+
 def warn(message):
     logger.warn(message)
+
+
 def error(message):
     logger.error(message)
+
+
 def critical(message):
     logger.critical(message)
