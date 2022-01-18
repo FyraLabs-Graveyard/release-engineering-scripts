41083a00242b3c09eceb1894132d4ea8d6fa58fe
Cappy Ishihara <cappy@cappuchino.xyz>
2022-01-18 12:03:29 +0700

    h

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Release Engineering Scripts
 
+A collection of release engineering scripts to make our lives easier when working on Ultramarine releases. Essentially an extension of umpkg but for the whole release instead of just one package.
+
+This is written for the Ultramarine Linux Release Engineering team, and (possibly) future forks of it. It is not intended for general use, but for developers.
+
+Everything is licensed under the [MIT license](LICENSE)
+
+## Scripts
+- monosplit: Splits an RPM spec monorepo into individual git repositories. (Can also be used to quickly create a new umpkg repo.)
+- massrebuild: Does a mass rebuild from one Koji tag to another. Useful for bumping versions. (only use every half year, because Fedora upstream)
+
+## Ansible Playbooks
+- compose: Composes a release from a koji tag (run this on a background terminal, it will take a while, and waste some space)
---
+++ monosplit.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+# RPM monorepo splitter script
+
+# from the Ultramarine Release Engineering script collection
+
+# Splits an RPM monorepo into multiple RPM spec repos.
+# Made for use with umpkg (https://gitlab.ultramarine-linux.org/release-engineering/umpkg) and GitLab
+
+# as always, everything is under the MIT license (https://opensource.org/licenses/MIT)
+# Copyright (c) 2022, Cappy Ishihara and Co. Do as you will as long as you credit me.
+# Written mostly with the help of GitHub Copilot (https://copilot.github.com), becuase looking up documentation is a waste of time. Comes with the added benefit of this entire script being annotated!
+
+# Your file structure should look like this:
+#  - foo.spec
+#  - bar.spec
+#  - patches/
+#       - foo/
+#          foo.patch
+#       - bar/
+#          bar.patch
+
+# EXISTING REPOS WILL BE OVERWRITTEN
+
+import os
+import sys
+import gitlab
+import configparser
+
+from itsdangerous import exc
+
+config = configparser.ConfigParser()
+
+token='' # Reminder to NEVER push your token to a public repo or we'll get hacked, I will dox you (jk)
+instance='https://gitlab.ultramarine-linux.org'
+branch='um36'
+gl = gitlab.Gitlab(url=instance, private_token=token)
+
+# Batch create gitlab project in dist-pkgs/
+
+# for each .spec file
+for file in os.listdir('.'):
+    if file.endswith('.spec'):
+        print('found spec file: ' + file)
+        # get the file name without the .spec
+        name = file.split('.')[0]
+        # create the project in group 'dist-pkgs' which id is 8
+        try:
+            print('Creating project: ' + name)
+            projectc = gl.projects.create({'name': name, 'namespace_id': 8})
+            print(projectc.get_id())
+            # get the project id of the new project
+            projectid=projectc.get_id()
+        except gitlab.exceptions.GitlabCreateError as e:
+            # if project has already been taken, continue pushing
+            # the error is a json object
+            print(e)
+            error = str(e)
+            if 'has already been taken' in error:
+                print(e)
+                print('Project already exists, continuing')
+                # get the project ID from path dist-pkgs/{name}
+                project = gl.projects.list(search=name)[0]
+                print(project.get_id())
+                projectid = project.get_id()
+                pass
+            else:
+                print(e)
+                continue
+        print('Created project: ' + name)
+        # create a folder here with the name of the project
+        print('Creating folder: ' + name)
+        os.mkdir(name)
+        # move the spec file to the folder
+        print('Moving spec file: ' + file)
+        os.rename(file, name + '/' + file)
+        # look in the patches/folder for a folder with the same name as the project
+        print('Looking for patches in patches/' + name)
+        for patch in os.listdir('patches'):
+            if patch == name:
+                # move all the files in the folder to the project folder
+                for file in os.listdir('patches/' + patch):
+                    os.rename('patches/' + patch + '/' + file, name + '/' + file)
+                    print('Moved file: ' + file)
+            # if the folder doesnt exist, skip it
+            else:
+                continue
+        # create the git repo in the project folder
+        os.chdir(name)
+        # make a config file with a [umpkg] section
+        # get the https url of the project
+        http_url = gl.projects.get(projectid).http_url_to_repo
+        print('Creating config file')
+        with open('umpkg.cfg', 'w') as configfile:
+            try:
+                config.add_section('umpkg')
+            except Exception as e:
+                print(e)
+                pass
+            config.set('umpkg', 'git_repo' , f'https://gitlab.ultramarine-linux.org/dist-pkgs/{name}.git')
+            # write the config file as umpkg.cfg
+            config.write(configfile)
+        print('Creating git repo')
+        os.system('git init --initial-branch=main')
+        os.system('git add .')
+        os.system('git commit -m "Automated project split from RPM monorepo by batchcreate.py"')
+        # get the SSH url of the project
+        url = f'ssh://git@gitlab.ultramarine-linux.org:2222/dist-pkgs/{name}.git'
+        os.system('git remote add origin ' + url)
+        # create a branch in the git repo with the name of the branch variable
+        print('push to remote')
+        os.system('git branch ' + branch)
+        os.system('git push --force --set-upstream origin ' + 'main')
+        # move back to the root folder
+        os.chdir('..')
